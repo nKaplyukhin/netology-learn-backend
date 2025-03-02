32a6295b7bb99dfbe05871172a3d8840c308606b
nKaplyukhin <nikita-140294@mail.ru>
2025-03-02 15:54:17 +0700

    lesson 3 done

--- exercise1.js
+++ exercise1.js
@@ -1,61 +1,44 @@
-
+const http = require("http");
 const yargs = require("yargs/yargs");
 const { hideBin } = require("yargs/helpers");
-const readline = require("node:readline");
-const { stdin: input, stdout: output } = require("node:process");
-const fs = require("fs")
-const rl = readline.createInterface({ input, output, });
 
-const FILE_EXT_WITH_DOT = ".txt"
+const apiKey = process.env.API_KEY;
 
-const argv = yargs(hideBin(process.argv)).option("result", {
-  alias: "R",
-  type: "string",
-  default: "result",
-}).help().argv
+const argv = yargs(hideBin(process.argv))
+  .option("city", {
+    alias: "C",
+    type: "string",
+    default: "krasnoyarsk",
+  })
+  .help().argv;
 
-const filename = `${argv.result}${FILE_EXT_WITH_DOT}`
+const query = argv.city || process.env.DEFAULT_QUERY;
 
-const getLastStepCounter = () => {
-  const data = fs.readFileSync(filename)
-  return data.toString().split("\n").filter(item => item.length).length || 0
-}
+const url = `http://api.weatherstack.com/current?access_key=${apiKey}&query=${query}`;
 
-let stepCounter = getLastStepCounter();
-
-const askQuestion = () => {
-  const randomData = Math.floor(Math.random() * 2) + 1;
-  let isWin = false
-
-  rl.question("Выбери Орел(1) или решка(2): ", (answer) => {
-    const answerNumber = +answer;
-    stepCounter += 1;
-
-    if (answerNumber === randomData) {
-      console.log(`Верно`);
-      isWin = true
-    } else {
-      isWin = false
-      console.log(`Неверно`);
+http
+  .get(url, (res) => {
+    const { statusCode } = res;
+    if (statusCode !== 200) {
+      console.log(statusCode);
+      return;
     }
-    const gameInfo = `${stepCounter}: ${isWin ? "Выйграл" : "Проиграл"}\n`
 
-    fs.appendFile(filename, gameInfo, (err) => {
-      if (err) {
-        console.warn(err);
-      }
-    })
+    res.setEncoding("utf-8");
 
-    rl.question("Сыграть еще\n1-да\n2-нет\n", (answer2) => {
-      const answerNumber = +answer2;
-      if (answerNumber === 1) {
-        askQuestion();
-        return
-      }
-      rl.close();
-      return
-    })
+    let data = "";
+
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      const parsedData = JSON.parse(data);
+      const temperature = parsedData.current.temperature;
+      const city = parsedData.location.name || query;
+      console.log(`Temperature in ${city}: ${temperature}°C`);
+    });
+  })
+  .on("error", (error) => {
+    console.error(error);
   });
-};
-
-askQuestion();
